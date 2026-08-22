b61ce1c230f314e90db4b3f810d01c7a8df6354d
Henry Peralta Briceño <mahe2di@gmail.com>
2026-08-21 22:09:55 -0500

    fix: allow Render frontend in CORS

--- backend/src/main/java/com/g9_latam_team_67/backend/config/CorsConfig.java
+++ backend/src/main/java/com/g9_latam_team_67/backend/config/CorsConfig.java
@@ -4,19 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration // Le dice a spring que es una clase de configuración
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
- 		   "http://localhost:5173",
-		   "http://157.137.221.198:3000"
-		)
+                        "http://localhost:5173",
+                        "http://157.137.221.198:3000",
+                        "https://techmind-frontend-xpeh.onrender.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
